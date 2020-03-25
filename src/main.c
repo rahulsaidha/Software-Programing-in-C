f9f5651eb2a5af90b512f9e57b7185214a7a7434
PCoser <44307690+PCoser@users.noreply.github.com>
2020-03-25 12:01:21 -0400

    Update main.c
    Added struct VEHICLE_STATUS;
    Defined devices addresses

--- src/main.c
+++ src/main.c
@@ -1,4 +1,34 @@
 /*Main routine*/
+#include <stdio.h>
+
+// Bus Addresses:
+#define   CTRL_ADDRESS    	    	= 10001010
+#define   GAS_PEDAL_SSR_ADDRESS		= 10000001
+#define   BRAKE_PEDAL_SSR_ADDRESS	= 10000010
+#define   STEERING_SSR_ADDRESS    	= 10000011
+#define   WHEEL_SSR_ADDRESS             = 10000100
+#define   DIR_ACT_ADDRESS		= 10000101
+#define   FUEL_ACT_ADDRESS		= 10000110
+#define   BRAKE_ACT_ADDRESS		= 10000111
+}
+
+struct VEHICLE_STATUS {
+    double vehicle_wheel_angle;
+    double vehicle_wheel_rotation;
+    double vehicle_position_X;
+    double vehicle_position_Y;
+    double motor_rotation;
+    double gas_pedal_pos;
+    double brake_pedal_pos;
+    double steering_wheel_pos;
+    double direction_actuator_pos;
+    double fuel_actuator_pos;
+    double brake_actuator_pos;
+    double range_sensor_val;
+    double vision_system_val;
+    int Comm_bus_address;
+    float Comm_bus_message;
+};
 
 void simulator(void) {
     differential_time();
@@ -9,7 +39,6 @@ void simulator(void) {
     driver_attitude();
     // environment_status(); release 3
     information_display();
-    
 }
 
 void system(void) {
@@ -26,6 +55,7 @@ void system(void) {
     
 int main(void)
 {
+    VEHICLE_STATUS vehicle_status;
     int iteration = 10; //TEMP
     while (1) {
         simulator();
