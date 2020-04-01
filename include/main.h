c8c263c7623a879b2064298b2eb7fd462c3bf12f
PCoser <44307690+PCoser@users.noreply.github.com>
2020-04-01 10:31:32 -0400

    Create main.h
    First version

---
+++ include/main.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// Bus Addresses:
+#define   CTRL_ADDRESS    	    	= 10001010
+#define   GAS_PEDAL_SSR_ADDRESS		= 10000001
+#define   BRAKE_PEDAL_SSR_ADDRESS	= 10000010
+#define   STEERING_SSR_ADDRESS    	= 10000011
+#define   WHEEL_SSR_ADDRESS         = 10000100
+#define   DIR_ACT_ADDRESS			= 10000101
+#define   FUEL_ACT_ADDRESS			= 10000110
+#define   BRAKE_ACT_ADDRESS			= 10000111
+
+#define PI 3.142
+
+struct VEHICLE_STATUS {
+    double vehicle_wheel_angle;
+    double vehicle_wheel_rotation;
+    double vehicle_position_X;
+    double vehicle_position_Y;
+    double motor_rotation;
+    double gas_pedal_pos;
+    double brake_pedal_pos;
+    double steering_wheel_pos;
+    double direction_actuator_pos;
+    double fuel_actuator_pos;
+    double brake_actuator_pos;
+    double range_sensor_val;
+    double vision_system_val;
+
+	int Comm_bus_address;
+    float Comm_bus_message;
+}
