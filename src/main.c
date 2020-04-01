3e6758b6823bc32599f27212f04f9f3a9a730fb1
PCoser <44307690+PCoser@users.noreply.github.com>
2020-04-01 10:30:51 -0400

    Update main.c
    included sampling time in the simulator;
    removed definitions to main.h;

--- src/main.c
+++ src/main.c
@@ -2,43 +2,16 @@
 
 
 */
-#include <stdio.h>
-
-// Bus Addresses:
-#define   CTRL_ADDRESS    	    	= 10001010
-#define   GAS_PEDAL_SSR_ADDRESS		= 10000001
-#define   BRAKE_PEDAL_SSR_ADDRESS	= 10000010
-#define   STEERING_SSR_ADDRESS    	= 10000011
-#define   WHEEL_SSR_ADDRESS         = 10000100
-#define   DIR_ACT_ADDRESS			= 10000101
-#define   FUEL_ACT_ADDRESS			= 10000110
-#define   BRAKE_ACT_ADDRESS			= 10000111
-
-struct VEHICLE_STATUS {
-    double vehicle_wheel_angle;
-    double vehicle_wheel_rotation;
-    double vehicle_position_X;
-    double vehicle_position_Y;
-    double motor_rotation;
-    double gas_pedal_pos;
-    double brake_pedal_pos;
-    double steering_wheel_pos;
-    double direction_actuator_pos;
-    double fuel_actuator_pos;
-    double brake_actuator_pos;
-    double range_sensor_val;
-    double vision_system_val;
-
-	int Comm_bus_address;
-    float Comm_bus_message;
-}
 
 void simulator(struct VEHICLE_STATUS *vehicle_status) {
-    differential_time(vehicle_status);
-    vehicle_angle(vehicle_status);
-    motor_rotation(vehicle_status);
-    vehicle_velocity(vehicle_status);
-    position_integration(vehicle_status);
+	static double total_time = 0;
+	double *time_sampling;
+	*time_sampling = differential_time();
+	total_time = total_time + *time_sampling;
+    vehicle_angle(vehicle_status,*time_sampling);
+    motor_rotation(vehicle_status,*time_sampling);
+    vehicle_velocity(vehicle_status,*time_sampling);
+    position_integration(vehicle_status,*time_sampling);
     driver_attitude(vehicle_status);
     // environment_status(vehicle_status); release 3
     information_display(vehicle_status);
