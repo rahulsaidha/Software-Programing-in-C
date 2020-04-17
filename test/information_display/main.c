b4161fbd2f6b405bb90efbaa993dcb40c7a4fb85
PCoser <44307690+PCoser@users.noreply.github.com>
2020-04-17 11:55:32 -0400

    Create main.c
    test main

---
+++ test/information_display/main.c
@@ -0,0 +1,52 @@
+#include "header.h"
+#include <string.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+void information_display(char *line, struct VEHICLE_STATUS* vehicle);
+
+int main(){
+    // Define Variables
+	VEHICLE_STATUS *vehicle;
+    vehicle = (struct VEHICLE_STATUS*) malloc(sizeof(struct VEHICLE_STATUS));
+    double time_sampling = 0.01;
+
+    // Open Log file
+    FILE *fp;
+    char str[100];
+    char* filename = "log.txt";
+    fp = fopen(filename, "w");
+    if (fp == NULL){
+        printf("Could not open file %s",filename);
+        return 1;
+    }
+
+    // Open Trajectory
+    FILE *trjc;
+    char* file_traj = "Trajectory.csv"; 
+    trjc = fopen(file_traj,"r");
+    if (trjc == NULL){
+        printf("Could not open file %s",file_traj);
+        return 1;
+    }
+    
+    char line[50];
+    int k = 0;
+    do{
+        fgets(line, 100,trjc);
+        if(k%1000 == 0){
+            information_display(line, vehicle);    
+            sleep(1);        
+        }
+        
+	}while(!feof(trjc));
+
+    printf("Simulation finished\nPress any key to close\n");
+	getchar();
+    fclose(fp);
+    fclose(trjc);
+}
